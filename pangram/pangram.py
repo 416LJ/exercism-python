8a643af78d0a1b20ad3ed240761ac33952ab07bb
416lj <laxsan.jeya@gmail.com>
2025-06-15 04:13:09 -0400

    submitted pangram, started isogram

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,9 @@
+import string
+
 def is_pangram(sentence):
-    pass
+    alphabets = string.ascii_lowercase[:26]
+    sentence = sentence.lower()
+    sentence = list(sentence.replace(' ',''))
+    return all(item in sentence for item in alphabets)
+
+print(is_pangram("hello world"))
