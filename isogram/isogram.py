3761bf376914023ec4124b427890d43d225940a2
416lj <laxsan.jeya@gmail.com>
2025-06-15 05:41:03 -0400

    submitted isogram, started isbn verifier

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,22 @@
+import string
 def is_isogram(string):
-    pass
+    chars = []
+    copy_word = []
+    if string == "":
+        return True
+    else:
+        word = string.lower()
+        word = sorted(list(word.replace(' ','')))
+        print(word)
+        for letter in (word):
+            if not letter.isalnum():
+                chars.append(letter)
+            else:
+                copy_word.append(letter)
+        copy_word = sorted(list(set(copy_word))+chars)
+        return copy_word == word
+
+
+
+
+print(is_isogram("six-year-old"))
