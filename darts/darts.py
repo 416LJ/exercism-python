a29f76ee768bf73df3ef21b336f84fd969ac3cc4
416lj <laxsan.jeya@gmail.com>
2025-06-15 22:53:27 -0400

    submitted darts

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,30 @@
+import math
+
 def score(x, y):
-    pass
+    res = math.hypot(x, y)
+    if res <= 10:
+        if x >= 10 or x <= -10 or y >= 10 or y <= -10:
+            return 1
+        elif x > 5 or x < -5 or y > 5 or y< -5:
+            return 1
+        elif x > 1 or x < -1 or y > 1 or y < -1:
+            if res > 5:
+                return 1
+            else:
+                return 5
+        elif x < 1 or x > -1 or y < 1 or y > -1:
+            if res > 1:
+                return 5
+            else:
+                return 10
+    else:
+        return 0
+
+
+print(score(-3.6,-3.6))
+
+
+
+print(math.hypot(-3.6, -3.6))
+
+
---
+++ darts/test.py
@@ -0,0 +1,4 @@
+print(score(6,6))
+print(score(15,26))
+print(score(2.2,2.6))
+print(score(0,-1))
