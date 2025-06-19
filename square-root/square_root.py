31cdaf3569c892ffa4252b8a2af44a95cb1f15b4
416lj <laxsan.jeya@gmail.com>
2025-06-19 00:37:53 -0400

    submitted square root

---
+++ square-root/square_root.py
@@ -0,0 +1,33 @@
+def square_root(number):
+    if number == 1:
+        return 1
+    
+    iterations = 0
+    left = 2
+    right = number-1
+
+    while left <= right:
+        iterations += 1
+        mid = (left + right) // 2
+
+        if mid*mid == number:
+            print(f"found sq rt of {number} : {mid} in {iterations} attempts")
+            return mid
+        
+        elif mid*mid < number:
+            left = mid + 1
+
+        else:
+            right = mid - 1
+        
+number = 999*999
+square_root(number)
+
+def square_root2(number):
+    iterations = 0
+    for i in range(1,number):
+            iterations += 1
+            if i*i == number:
+                return f"found sq rt of {number} : {i} in {iterations} attempts"
+
+print(square_root2(number))
