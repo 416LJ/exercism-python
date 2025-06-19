b35ed4b4db3bb65ad9cfb8dbe90da4c6a726498a
416lj <laxsan.jeya@gmail.com>
2025-06-18 20:42:10 -0400

    submitted atbash cipher

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,6 +1,32 @@
-def encode(plain_text):
-    pass
+plain = list("abcdefghijklmnopqrstuvwxyz1234567890")
+cypher = list("zyxwvutsrqponmlkjihgfedcba1234567890")
+translator = {plain[i] :  cypher[i] for i in range(len(plain))}
 
+def encode(plain_text):
+    temp = list(plain_text.lower().replace(" ", ""))
+    encoded = []
+    encoded_str = ""
+    for i in temp:
+        if i.isalnum():
+            encoded.append(translator[i])
+
+    print(encoded)
+    encoded_str = [''.join(encoded[variable:variable+5]) for variable in range(0, len(encoded),5)]
+    encoded_str = ' '.join(encoded_str)
+    #print(encoded_str)
+    return encoded_str
 
 def decode(ciphered_text):
-    pass
+    temp = list(ciphered_text.lower().replace(" ", ""))
+    encoded = []
+    encoded_str = ""
+    for i in temp:
+        if i.isalnum():
+            encoded.append(translator[i])
+
+    print(encoded)
+    encoded_str = [''.join(encoded[variable:variable+5]) for variable in range(0, len(encoded),5)]
+    encoded_str = ' '.join(encoded_str)
+    #print(encoded_str)
+    return encoded_str.replace(" ","")
+
